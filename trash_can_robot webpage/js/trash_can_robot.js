dbed435228c41847423330773e91d7731b0e9f29
Yu-Yun Chang <yunchang1986@gmail.com>
2018-05-17 17:05:37 +0800

    加入物件移動方法
    讓垃圾桶可以移動，但目前有bug

--- trash_can_robot webpage/js/trash_can_robot.js
+++ trash_can_robot webpage/js/trash_can_robot.js
@@ -4,65 +4,149 @@
 */
 
 /* Global Initialization */
-var members = [ "Moon", "Liao", "Jacky", "Audrey", "Mag", "Hank", "Kane", "Kouichi", "Rich", "Sheng", "Yuyun" ];
+var c = document.getElementById("mycan");
+var ctx = c.getContext("2d");
+var members = ["Moon", "Liao", "Jacky", "Audrey", "Mag", "Hank", "Kane", "Kouichi", "Rich", "Sheng", "Yuyun"];
 var isBusy = false;
 var checkLoop = setInterval(CheckStatus, 500); //Check Status with a timer for each 0.5 sec.
 
+var fps = 60;
+var percent = 0
+var direction = 1;
 
 
-function CheckStatus(){
+
+function CheckStatus() {
   // @TODO: fetch status from API (currently using a random function)
-  var num = Math.floor(Math.random()*100);
-  if( num <= 10){
-	   isBusy = false;
-    //  路徑消失
-     ctx.clearRect(0,0,c.width,c.height)
+  var num = Math.floor(Math.random() * 100);
+  if (num <= 10) {
+    isBusy = false;
   }
 
   // show images according status
-  if(isBusy){
-	for(var i in members){
-	  var person = members[i];
-	  document.getElementById(person).disabled = true;
-	}
-  }else{
-	for(var i in members){
-	  var person = members[i];
-	  document.getElementById(person).disabled = false;
-	}
+  if (isBusy) {//垃圾桶工作中
+    for (var i in members) {
+      var person = members[i];
+      document.getElementById(person).disabled = true;
+    }
+  } else { //垃圾桶無人使用時
+    for (var i in members) {
+      var person = members[i];
+      document.getElementById(person).disabled = false;
+      //  路徑消失
+      ctx.clearRect(0, 0, c.width, c.height)
+    }
   }
   return isBusy;
 }
 
 
-function SendRequest(name){
+function SendRequest(name) {
   isBusy = CheckStatus();
-  var endPoint= ReturnEndPointByID(name);
-  if(!isBusy){
-    ShowPath(214.5,750,endPoint[0],endPoint[1]);
+  var endPoint = ReturnEndPointByID(name);
+  if (!isBusy) {
+    MakeCanMove(endPoint); //moving the trashcan icon
+    ShowPath(214.5, 750, endPoint[0], endPoint[1]);
+    
+    //讓
+    //MakeCanMove();
 
-	// @TODO: send request via API
+    // @TODO: send request via API
 
-	// @TODO: high-light target
+    // @TODO: high-light target
 
-	// check status
-	isBusy = true;
-	CheckStatus();
+    // check status
+    isBusy = true;
+    CheckStatus();
   }
 }
 
-function ShowPath(start_x, start_y, end_x, end_y){
-
+function ShowPath(start_x, start_y, end_x, end_y) {
   ctx.beginPath();
-  ctx.lineWidth="5";
-  ctx.strokeStyle="green"; // Green path
-  ctx.moveTo(start_x,start_y);
-  ctx.lineTo(end_x,end_y);
+  ctx.lineWidth = "5";
+  ctx.strokeStyle = "green"; // Green path
+  ctx.moveTo(start_x, start_y);
+  ctx.lineTo(end_x, end_y);
   ctx.stroke(); // Draw it
-
 }
 
-function ReturnEndPointByID(name){
+
+
+
+
+function MakeCanMove(endPoint) {
+
+  // set the animation position (0-100)
+  percent += direction;
+  if (percent < 0) {
+    percent = 0;
+    direction = 1;
+  };
+  if (percent > 100) {
+    percent = 100;
+    direction = -1;
+  };
+  var newPoint = endPoint;
+  draw(percent, 214.5, 750, endPoint[0], endPoint[1]);
+  //console.log(endPoint[0]);
+  //console.log(endPoint[1]);
+
+  // request another frame
+  setTimeout(function () {
+    requestAnimationFrame(MakeCanMove(newPoint));
+  }, 1000 / fps);
+}
+
+
+// draw the current frame based on sliderValue
+function draw(sliderValue, start_x, start_y, end_x, end_y) {
+
+  // draw the tracking rectangle
+  var xy;
+
+  if (sliderValue < 25) {
+    var percent = sliderValue / 24;
+  
+    xy = getLineXYatPercent(
+      { x: 214.5, y: 750},
+      { x: end_x, y: end_y}, 
+      percent
+    );
+    }
+    //console.log(end_x);
+    //console.log(end_y);
+    console.log(xy);
+  drawRect(xy);
+}
+
+
+// draw tracking rect at xy
+function drawRect(point) {
+  
+  var img = document.getElementById("scream");
+  ctx.drawImage(img,point.x-30,point.y-30);
+   ctx.fillStyle = "yellow";
+  ctx.strokeStyle = "gray";
+  ctx.lineWidth = 1;
+  ctx.beginPath();
+  //ctx.rect(point.x, point.y, 25, 15);
+  ctx.fill();
+  ctx.stroke();
+}
+
+// line: percent is 0-1
+function getLineXYatPercent(startPt, endPt, percent) {
+  var dx = endPt.x - startPt.x;
+  var dy = endPt.y - startPt.y;
+  var X = startPt.x + dx * percent;
+  var Y = startPt.y + dy * percent;
+  return ({
+    x: X,
+    y: Y
+  });
+}
+
+function ReturnEndPointByID(name) {
   /*
     左上轉折點 : 214.5,130
     Jacky,Andrey: 214.5,280
@@ -76,7 +160,7 @@ function ReturnEndPointByID(name){
     Yuyun:  620,680
     右下轉折點:  620,750
   */
-  var end_x,end_y;
+  var end_x, end_y;
 
   switch (name) {
     case 'Moon': end_x = 214.5; end_y = 130; break;
@@ -92,5 +176,5 @@ function ReturnEndPointByID(name){
     case 'Yuyun': end_x = 620; end_y = 680; break;
     default: break;
   }
-  return [end_x,end_y];
+  return [end_x, end_y];
 }
